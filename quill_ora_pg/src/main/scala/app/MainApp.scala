4c5005c349a0320feb429fe7310f272a7f5417e8
yakushev <ugr@bk.ru>
2023-04-04 18:51:07 +0500

    ZLayer constructors composition in pg,ora. Using logic.

--- quill_ora_pg/src/main/scala/app/MainApp.scala
+++ quill_ora_pg/src/main/scala/app/MainApp.scala
@@ -4,11 +4,13 @@ import app.MainApp.mainApp
 import common.Person
 import io.getquill.SnakeCase
 import io.getquill.jdbczio.Quill
+import io.getquill.jdbczio.Quill.{Oracle, Postgres}
 import ora_service.OraDataService
 import pg_service.PgDataService
 import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, ZLayer}
 
 import java.sql.SQLException
+import javax.sql.DataSource
 
 object MainApp extends ZIOAppDefault {
 
@@ -24,15 +26,44 @@ object MainApp extends ZIOAppDefault {
     _ <-ZIO.foreachDiscard(personsAll){p => ZIO.logInfo(s"Person [ORA]: ${p.name} with ${p.age}")}
   } yield ()
 
+  val logic :ZIO[PgDataService with OraDataService,SQLException,Unit] = for {
+    pgService <- ZIO.service[PgDataService]
+    oraService <- ZIO.service[OraDataService]
+    personsPgAll <- pgService.getPeopleAll
+    personsOraAll <- oraService.getPeopleAll
+    _ <- ZIO.foreachDiscard(personsPgAll){p => ZIO.logInfo(s"Person [PG]: ${p.name} with ${p.age}")}
+    _ <- ZIO.foreachDiscard(personsOraAll){p => ZIO.logInfo(s"Person [ORA]: ${p.name} with ${p.age}")}
+  } yield ()
+
+  val dsPgLayer: ZLayer[Any, Throwable, DataSource] = Quill.DataSource.fromPrefix("pgConfig")
+  val quillPgLayer: ZLayer[DataSource, Nothing, Postgres[SnakeCase.type]] = Quill.Postgres.fromNamingStrategy(SnakeCase)
+  val dsPgServ: ZLayer[Postgres[SnakeCase], Nothing, PgDataService] = PgDataService.live
+
+  val dsOraLayer: ZLayer[Any, Throwable, DataSource] = Quill.DataSource.fromPrefix("oraConfig")
+  val quillOraLayer: ZLayer[DataSource, Nothing, Oracle[SnakeCase.type]] = Quill.Oracle.fromNamingStrategy(SnakeCase)
+  val dsOraServ: ZLayer[Oracle[SnakeCase], Nothing, OraDataService] = OraDataService.live
+
+
+  val pg :ZLayer[Any,Throwable,PgDataService] = dsPgLayer >>> quillPgLayer >>> dsPgServ
+  val ora: ZLayer[Any,Throwable,OraDataService] = dsOraLayer >>> quillOraLayer >>> dsOraServ
+
   val mainApp: ZIO[ZIOAppArgs, Throwable, Unit] = for {
-    _ <- pgData.provide(PgDataService.live,
-      Quill.Postgres.fromNamingStrategy(SnakeCase),
-      Quill.DataSource.fromPrefix("pgConfig")
+/*    _ <- pgData.provide(
+      dsPgServ,
+      quillPgLayer,
+      dsPgLayer
     )
-    _ <- oraData.provide(OraDataService.live,
-      Quill.Oracle.fromNamingStrategy(SnakeCase),
-      Quill.DataSource.fromPrefix("oraConfig")
+    _ <- oraData.provide(
+      dsOraServ,
+      quillOraLayer,
+      dsOraLayer
+    )*/
+
+    _ <- logic.provide(
+      pg,
+      ora
     )
+
   } yield ()
 
   def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
